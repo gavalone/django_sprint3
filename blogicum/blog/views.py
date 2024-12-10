0c3f57f4a58f290710124e8f452ac23b0cb82823
Diana Ermolaeva <gavalonee@gmail.com>
2024-12-10 21:44:41 +0300

    Second part

---
+++ blogicum/blog/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post, Category, Location
+from django.utils.timezone import now
+
+
+def index(request):
+    template = 'blog/index.html'
+    post_list = Post.objects.select_related('category').filter(
+        is_published=True,
+        pub_date__lte=now(),
+        category__is_published=True
+    ).order_by('-pub_date')[:5]
+    context = {'post_list': post_list}
+    return render(request, template, context)
+
+
+def post_detail(request, id):
+    template = 'blog/detail.html'
+    post = get_object_or_404(
+        Post.objects.filter(
+            pub_date__lte=now(),
+            category__is_published=True,
+            is_published=True
+        ),
+        id=id
+    )
+    context = {'post': post}
+    return render(request, template, context)
+
+
+def category_posts(request, category_slug):
+    template = 'blog/category.html'
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True)
+    post_list = Post.objects.filter(
+        category=category,
+        is_published=True,
+        pub_date__lte=now(),
+    ).order_by('-pub_date')
+    context = {
+        'category': category,
+        'post_list': post_list,
+    }
+    return render(request, template, context)
